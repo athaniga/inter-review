9517787e36c8b0be7a42c4ec7404084cdccb51a4
athaniga <56320575+athaniga@users.noreply.github.com>
2021-02-18 14:47:19 -0600

    Initial commit

---
+++ src/main/java/com/interreview/InterReviewApplication.java
@@ -0,0 +1,13 @@
+package com.interreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterReviewApplication.class, args);
+    }
+
+}
