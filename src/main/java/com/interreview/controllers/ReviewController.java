46faf1a39eeed2b48f9246df830123593da07052
athaniga <56320575+athaniga@users.noreply.github.com>
2021-03-25 15:27:56 -0500

    Delete Modal Added
    Delete function implemented with modal confirming delete.
    
    However there is a bug where upon deletion, the first entry in the review list will be deleted instead of the one selected.

--- src/main/java/com/interreview/controllers/ReviewController.java
+++ src/main/java/com/interreview/controllers/ReviewController.java
@@ -70,7 +70,7 @@ public class ReviewController {
     }
 
     @GetMapping("/delete/{id}")
-    public String deleteReview(@PathVariable long id) {
+    public String deleteReview(@PathVariable Long id) {
         this.interviewRepo.deleteById(id);
         return "redirect:/reviews";
     }
